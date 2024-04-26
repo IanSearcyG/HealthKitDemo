e4f0c2572c66162ab0ced27dd47e10d8204dd488
Ian Searcy-Gardner <ian@searcygardner.com>
2024-04-26 10:00:53 -0700

    minor changes

--- HealthKitDemo/ContentView.swift
+++ HealthKitDemo/ContentView.swift
@@ -6,19 +6,173 @@
 //
 
 import SwiftUI
+import HealthKit
+import HealthKit
+import Charts
 
 struct ContentView: View {
+    @State private var steps: [Step] = []
+    @State private var useMockData: Bool = false
+    @State var bob = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                VStack {
+                    
+                    Toggle("Use Mock Data", isOn: $useMockData)
+                        .padding()
+                    
+                    Text("Past 30 Days")
+                    
+                    
+                    
+                    if steps.isEmpty {
+                        Text("No data loaded yet")
+                            .foregroundColor(.gray)
+                            .font(.title)
+                            .padding()
+                    } else {
+                        Chart {
+                            ForEach(steps, id: \.id) { step in
+                                BarMark(
+                                    x: .value("Date", step.formattedDate),
+                                    y: .value("Steps", step.count)
+                                )
+                            }
+                        }
+                        .chartXAxis {
+                            AxisMarks(values: .automatic(desiredCount: 5)) {
+                                AxisGridLine()
+                                AxisTick()
+                            }
+                        }
+                        .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.width * 0.8)
+                        .padding(.bottom, 30)
+                        Chart {
+                            ForEach(steps, id: \.id) { step in
+                                LineMark(
+                                    x: .value("Date", step.formattedDate),
+                                    y: .value("Steps", step.count)
+                                )
+                                .interpolationMethod(.catmullRom)
+                            }
+                        }
+                        .chartXAxis {
+                            AxisMarks(values: .automatic(desiredCount: 5)) {
+                                AxisGridLine()
+                                AxisTick()
+                            }
+                        }
+                        .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.width * 0.5)
+                        .padding(.bottom, 30)
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                    
+                    
+                }
+                .navigationTitle("Healthkit Demo")
+            }
         }
-        .padding()
+        .onAppear {
+            #if targetEnvironment(simulator)
+            useMockData = true
+            #endif
+            if useMockData {
+                self.steps = self.mockSteps()
+                self.fetchStepsFromHealthKit()
+            } else {
+                self.fetchStepsFromHealthKit()
+            }
+        }
+        .onChange(of: useMockData) { newValue in
+            if newValue {
+                self.steps = self.mockSteps()
+            } else {
+                self.steps = []
+                self.fetchStepsFromHealthKit()
+            }
+        }
+        
+        
+    }
+    
+    private func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection) {
+        var newSteps: [Step] = []
+        let endDate = Date() // today
+        let startDate = Calendar.current.date(byAdding: .day, value: -29, to: endDate)! // 30 days ago
+        
+        statisticsCollection.enumerateStatistics(from: startDate, to: endDate) { statistics, stop in
+            let count = statistics.sumQuantity()?.doubleValue(for: .count())
+            let step = Step(count: Int(count ?? 0), date: statistics.startDate)
+            newSteps.append(step)
+        }
+        
+        DispatchQueue.main.async {
+            self.steps = newSteps
+        }
+    }
+    
+    
+    
+    func mockSteps() -> [Step] {
+        var steps: [Step] = []
+        let currentDate = Date()
+        let calendar = Calendar.current
+        
+        for day in (0..<30).reversed() {
+            if let date = calendar.date(byAdding: .day, value: -day, to: currentDate) {
+                let count = Int.random(in: 1000...10000)
+                let step = Step(count: count, date: date)
+                steps.append(step)
+            }
+        }
+        print("Mock steps generated: \(steps)")
+        return steps
+    }
+    
+    
+    
+    func fetchStepsFromHealthKit() {
+        #if targetEnvironment(simulator)
+        print("Running on the Simulator - HealthKit is not available")
+        #else
+        HealthDataManager.shared.requestAuthorization { success in
+            print("HealthKit Auth: \(success)")
+            if success && !useMockData {
+                let dailyInterval = DateComponents(day: 1)
+                HealthDataManager.shared.calculateSteps(interval: dailyInterval) { statisticsCollection in
+                    if let statisticsCollection = statisticsCollection {
+                        DispatchQueue.main.async {
+                            self.updateUIFromStatistics(statisticsCollection)
+                        }
+                    }
+                }
+            }
+        }
+        #endif
+    }
+    
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
-#Preview {
-    ContentView()
+struct Step: Identifiable {
+    let id = UUID()
+    let count: Int
+    let date: Date
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter.string(from: date)
+    }
 }
--- HealthKitDemo/HealthDataManager.swift
+++ HealthKitDemo/HealthDataManager.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import HealthKit
+
+final class HealthDataManager {
+    
+    static let shared = HealthDataManager()
+    var healthStore: HKHealthStore?
+    var query: HKStatisticsCollectionQuery?
+    let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    
+    private init() {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        guard let healthStore = healthStore else {
+            return completion(false)
+        }
+        healthStore.requestAuthorization(toShare: [], read: [stepType]) { success, error in
+            completion(success)
+        }
+    }
+    
+    func calculateSteps(interval: DateComponents, completion: @escaping (HKStatisticsCollection?) -> Void) {
+        guard let healthStore = healthStore else { return }
+        
+        let startDate = Calendar.current.date(byAdding: .year, value: -1, to: Date()) // Fetch data from the last year
+        let anchorDate = Date.mondayAt12AM() // Anchor date for the query
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictStartDate)
+        
+        query = HKStatisticsCollectionQuery(quantityType: stepType,
+                                            quantitySamplePredicate: predicate,
+                                            options: .cumulativeSum,
+                                            anchorDate: anchorDate,
+                                            intervalComponents: interval)
+        
+        query?.initialResultsHandler = { query, statisticsCollection, error in
+            completion(statisticsCollection)
+        }
+        
+        healthStore.execute(query!)
+    }
+    
+    // Helper function to get Monday at 12 AM for the anchor date
+    private static func mondayAt12AM() -> Date {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.firstWeekday = 2 // Set the week start to Monday
+        let now = Date()
+        let components = calendar.dateComponents([.year, .month, .weekday, .day], from: now)
+        let startOfWeek = calendar.date(from: components)!
+        let startOfDay = calendar.startOfDay(for: startOfWeek)
+        return startOfDay
+    }
+}
+
+extension Date {
+    static func mondayAt12AM() -> Date {
+        return Calendar(identifier: .iso8601).date(from: Calendar(identifier: .iso8601).dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date()))!
+    }
+}
