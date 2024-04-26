64e31e378bac34d370aef833baceeeaadaf35b6e
Ian Searcy-Gardner <ian@searcygardner.com>
2024-04-26 09:38:51 -0700

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by Ian Searcy-Gardner on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
